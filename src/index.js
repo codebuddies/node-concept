d6c23ae8676f9dd32ce16a0d079955a526ef5c6f
Angelo Cordon <angelocordon@gmail.com>
2019-09-01 19:30:06 -0700

    Add CORS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
+import "dotenv/config";
+import cors from "cors";
 import express from "express";
 
 const app = express();
+
+// Add CORS headers for allowing outside applications to be able to access
+// resources.  See library documentation for more options
+// (i.e., whitelisting domains): https://github.com/expressjs/cors
+app.use(cors());
 
 app.get("/", (req, res) => {
 	res.send("hello world");
